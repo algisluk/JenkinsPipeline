9bcb68ffad6805448c4879daecce633a303dfb1a
algisluk <algis.lukaszewicz@gmail.com>
2022-10-28 16:13:03 +0200

    Add files via upload

---
+++ JenkinsPipeline/JenkinsPipelineApp.swift
@@ -0,0 +1,17 @@
+//
+//  JenkinsPipelineApp.swift
+//  JenkinsPipeline
+//
+//  Created by Algis on 28/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct JenkinsPipelineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
